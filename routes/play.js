248d5818674d89b7dcdd7e6efd4bb372c3e47950
Vajahath Ahmed <vajuoff.1@gmail.com>
2016-04-12 20:43:44 +0530

    npm update in linux, UI redesign

--- routes/play.js
+++ routes/play.js
@@ -2,22 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 // require player
-var Player = require('player');
+// var Player = require('player');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.render('play', {
-		title: "Play | HomeWork"
-	});
+	// res.render('play', {
+	// 	title: "Play | HomeWork"
+	// });
 });
 
 // create player instance
-var player = new Player('http://mr4.douban.com/blablablabla/p1949332.mp3');
+// var player = new Player('http://mr4.douban.com/blablablabla/p1949332.mp3');
 
 // play now and callback when playend
-player.play(function(err, player){
-  console.log('playend!');
-});
+// player.play(function(err, player){
+//   console.log('playend!');
+// });
 
 
 module.exports = router;
