02452491d14d5999237aac2012c455fde32d77b5
Vajahath Ahmed <vajuoff.1@gmail.com>
2016-04-25 23:03:32 +0530

    added login function to some extent, but still console is not secure

---
+++ models/User.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var user = {
+	name: String,
+	email:{
+		type:String,
+		unique: true
+	},
+	psw: String,
+	id: {
+		type:String,
+		unique: true
+	}
+}
+
+var user_schema = mongoose.Schema(user, {collection:"User"});
+
+module.exports = mongoose.model('User', user_schema);
