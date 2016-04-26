604e6617c2683a11c98ca474432d2738321b95ec
Vajahath Ahmed <vajuoff.1@gmail.com>
2016-04-26 14:09:37 +0530

    angular connected

--- public/javascripts/console_app.js
+++ public/javascripts/console_app.js
@@ -21,26 +21,57 @@ app.factory('datastore', [function() {
 			outside: 30,
 			string: "Normal temperature inside."
 		},
-		doors: {
-			total: 3,
-			closed: 1
-		},
-		windows: {
-			closed: 6,
-			open: 3
-		},
 		water: {
 			level: 60, // in percentage. Empty should be calculated as 100-x
 			timeLeft: 20
 		},
-		devices: {
-			off: 13,
-			on: 2
+		rain: {
+			chance: 60,
+			status: "There can be rain"
 		},
-		rain:{
-			chance: 60%,
-			status:"There can be rain"
+		inverter: {
+			charge: 91,
+			status: "Charging"
+		},
+		users_online: 2,
+		partyMode: false,
+		doors: {
+			total: 3,
+			closed: 1,
+			list: {
+				door1: true,
+				door2: true
+			}
+		},
+		windows: {
+			closed: 6,
+			open: 3,
+			list: {
+				window1: true,
+				window2: true
+			}
+		},
+		devices: {
+			off: 13, //total
+			on: 2, //total
+			// list 
+			ac: "ON",
+			wash: "OFF",
+			fridge: "ON",
+			motor: "OFF",
+
+			lights: {
+				light1: "ON",
+				light2: "OFF",
+			},
+
+			fans: {
+				fan1: "ON",
+				fan2: "OFF",
+			}
 		}
+
+
 	}
 
 	return data;
