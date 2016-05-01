2437e7e1ce35f32938dcc8327d41c6b2ecf6ca2f
sanilkpch <sanilkumarp001@gmail.com>
2016-05-01 13:18:09 +0000

    remove deevice isnt working properly

--- models/Device.js
+++ models/Device.js
@@ -1,14 +1,24 @@
 var mongoose = require('mongoose');
 
+
 var device = {
+
+    };
+
+/*
+var device = {
+
 	device:{
 	        type: String,
 	        unique:true
 	        },
 	pin: Number
 	
-};
 
-var deviceSchema = mongoose.Schema(device);
+
+};
+*/
+
+var deviceSchema = mongoose.Schema(device,{collection:"device"});
 
 module.exports = mongoose.model('Device', deviceSchema);
