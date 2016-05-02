4626fe94b0b39219e615584edaf46584441b4b1c
Vajahath Ahmed <vajuoff.1@gmail.com>
2016-05-02 13:56:46 +0530

    testing another method

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ var pi_interface = require('../customLibs/pi_interface');
 var Gpio= require('onoff').Gpio;
 var User = require('../models/User');
 
+var io = require('socket.io');
+
 var router = express.Router();
 
 /* GET home page. */
